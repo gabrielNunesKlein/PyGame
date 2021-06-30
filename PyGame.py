49ce61b5aa813f08527e746f505af246f1e5b135
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-06-30 19:57:40 -0300

    Desenhando objetos

--- PyGame.py
+++ PyGame.py
@@ -4,8 +4,8 @@ from sys import exit
 
 pygame.init()
 
-largura = 640
-altura = 480
+largura = 640 #x
+altura = 480 #y
 
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Jogo')
@@ -15,5 +15,16 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             exit()
+    #Coredenadas x e y
+    #Desenha um retangulo na tela (tela, (r, g, b), (x, y, largura, altura))
+    pygame.draw.rect(tela, (255, 0, 0), (200, 300, 40, 50))
+
+    pygame.draw.rect(tela, (255, 255, 0), (10, 160, 40, 50))
+
+    #Desenha um circulo na tela (tela, (r, g, b), (x, y), raio_do_circulo)
+    pygame.draw.circle(tela, (0, 0, 120), (300, 260), 40)
+
+    #Desenha uma lina na tela (tela, (r, g, b), (posição Inicial), (Posição Final), px)
+    pygame.draw.line(tela, (255, 255, 0), (390, 0), (390, 600), 5)
     pygame.display.update()
 
